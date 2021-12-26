f1e414da08301b8e3797febceb6177b3a55c3de0
Jose Perez <77651693+josea2003@users.noreply.github.com>
2021-12-26 12:28:55 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const bp = require('body-parser')
+const app = express()
+const port = 3000
+
+app.use(bp.json())
+app.use(bp.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+	res.send('Hello World!')
+})
+
+app.post('/apple', (req, res) => {
+	console.log(req.body)
+	res.send(`Hello World! ${req.body.name}`);
+})
+
+app.listen(port, () => {
+	console.log(`Example app listening at http://localhost:${port}`)
+})
